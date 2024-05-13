4a3bd587cf31f0845f77541ff358a9e858597a75
Ahmed Talaat <ahmedtm2015@gmail.com>
2024-05-13 23:40:13 +0300

    attempts to add depts chart page 👌🐯

---
+++ Pages/ChartsPages/DepartmentsStatisticsPage.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace Spring.Pages.ChartsPages
+{
+    partial class DepartmentsStatisticsPage
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Syncfusion.Windows.Forms.Chart.ChartSeries chartSeries1 = new Syncfusion.Windows.Forms.Chart.ChartSeries();
+            Syncfusion.Windows.Forms.Chart.ChartCustomShapeInfo chartCustomShapeInfo1 = new Syncfusion.Windows.Forms.Chart.ChartCustomShapeInfo();
+            Syncfusion.Windows.Forms.Chart.ChartLineInfo chartLineInfo1 = new Syncfusion.Windows.Forms.Chart.ChartLineInfo();
+            this.chartControl1 = new Syncfusion.Windows.Forms.Chart.ChartControl();
+            this.progressBarAdvUsersStatistics = new Syncfusion.Windows.Forms.Tools.ProgressBarAdv();
+            ((System.ComponentModel.ISupportInitialize)(this.progressBarAdv3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.progressBarAdvUsersStatistics)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // progressBarAdv3
+            // 
+            this.progressBarAdv3.BackMultipleColors = new System.Drawing.Color[] {
+        System.Drawing.Color.Empty};
+            this.progressBarAdv3.Location = new System.Drawing.Point(0, 392);
+            this.progressBarAdv3.MultipleColors = new System.Drawing.Color[] {
+        System.Drawing.Color.Empty};
+            this.progressBarAdv3.Size = new System.Drawing.Size(693, 13);
+            this.progressBarAdv3.Text = "progressBarUsersStatistics";
+            // 
+            // chartControl1
+            // 
+            this.chartControl1.BackgroundImage = global::Spring.Properties.Resources.desktopGrid_f324974d;
+            this.chartControl1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.chartControl1.BackInterior = new Syncfusion.Drawing.BrushInfo(Syncfusion.Drawing.GradientStyle.Horizontal, new System.Drawing.Color[] {
+            System.Drawing.Color.FromArgb(((int)(((byte)(221)))), ((int)(((byte)(220)))), ((int)(((byte)(220))))),
+            System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(241)))), ((int)(((byte)(231))))),
+            System.Drawing.Color.FromArgb(((int)(((byte)(221)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))))});
+            this.chartControl1.ChartArea.BackInterior = new Syncfusion.Drawing.BrushInfo(System.Drawing.Color.Transparent);
+            this.chartControl1.ChartArea.CursorLocation = new System.Drawing.Point(0, 0);
+            this.chartControl1.ChartArea.CursorReDraw = false;
+            this.chartControl1.ChartInterior = new Syncfusion.Drawing.BrushInfo(System.Drawing.Color.Transparent);
+            this.chartControl1.CustomPalette = new System.Drawing.Color[] {
+        System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(69)))), ((int)(((byte)(153))))),
+        System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(44)))), ((int)(((byte)(36))))),
+        System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(88)))), ((int)(((byte)(167))))),
+        System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(95)))), ((int)(((byte)(47))))),
+        System.Drawing.Color.FromArgb(((int)(((byte)(203)))), ((int)(((byte)(83)))), ((int)(((byte)(27))))),
+        System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(31)))), ((int)(((byte)(64)))))};
+            this.chartControl1.DataSourceName = "[none]";
+            this.chartControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.chartControl1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(101)))), ((int)(((byte)(103)))), ((int)(((byte)(103)))));
+            this.chartControl1.IsWindowLess = false;
+            // 
+            // 
+            // 
+            this.chartControl1.Legend.Location = new System.Drawing.Point(628, 82);
+            this.chartControl1.LegendsPlacement = Syncfusion.Windows.Forms.Chart.ChartPlacement.Outside;
+            this.chartControl1.Localize = null;
+            this.chartControl1.Location = new System.Drawing.Point(0, 0);
+            this.chartControl1.Name = "chartControl1";
+            this.chartControl1.Palette = Syncfusion.Windows.Forms.Chart.ChartColorPalette.Custom;
+            this.chartControl1.PrimaryXAxis.GridLineType.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(158)))), ((int)(((byte)(205)))));
+            this.chartControl1.PrimaryXAxis.LineType.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(158)))), ((int)(((byte)(205)))));
+            this.chartControl1.PrimaryXAxis.LogLabelsDisplayMode = Syncfusion.Windows.Forms.Chart.LogLabelsDisplayMode.Default;
+            this.chartControl1.PrimaryXAxis.Margin = true;
+            this.chartControl1.PrimaryXAxis.TickColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(158)))), ((int)(((byte)(205)))));
+            this.chartControl1.PrimaryXAxis.TitleColor = System.Drawing.SystemColors.ControlText;
+            this.chartControl1.PrimaryYAxis.GridLineType.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(158)))), ((int)(((byte)(205)))));
+            this.chartControl1.PrimaryYAxis.LineType.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(158)))), ((int)(((byte)(205)))));
+            this.chartControl1.PrimaryYAxis.LogLabelsDisplayMode = Syncfusion.Windows.Forms.Chart.LogLabelsDisplayMode.Default;
+            this.chartControl1.PrimaryYAxis.Margin = true;
+            this.chartControl1.PrimaryYAxis.TickColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(158)))), ((int)(((byte)(205)))));
+            this.chartControl1.PrimaryYAxis.TitleColor = System.Drawing.SystemColors.ControlText;
+            chartSeries1.FancyToolTip.ResizeInsideSymbol = true;
+            chartSeries1.Name = "usrs";
+            chartSeries1.Points.Add(0D, ((double)(23D)));
+            chartSeries1.Points.Add(0D, ((double)(34D)));
+            chartSeries1.Points.Add(0D, ((double)(50D)));
+            chartSeries1.Points.Add(0D, ((double)(47D)));
+            chartSeries1.Resolution = 0D;
+            chartSeries1.StackingGroup = "Default Group";
+            chartSeries1.Style.AltTagFormat = "";
+            chartSeries1.Style.DrawTextShape = false;
+            chartLineInfo1.Alignment = System.Drawing.Drawing2D.PenAlignment.Center;
+            chartLineInfo1.Color = System.Drawing.SystemColors.ControlText;
+            chartLineInfo1.DashPattern = null;
+            chartLineInfo1.DashStyle = System.Drawing.Drawing2D.DashStyle.Solid;
+            chartLineInfo1.Width = 1F;
+            chartCustomShapeInfo1.Border = chartLineInfo1;
+            chartCustomShapeInfo1.Color = System.Drawing.SystemColors.HighlightText;
+            chartCustomShapeInfo1.Type = Syncfusion.Windows.Forms.Chart.ChartCustomShape.Square;
+            chartSeries1.Style.TextShape = chartCustomShapeInfo1;
+            chartSeries1.Text = "usrs";
+            this.chartControl1.Series.Add(chartSeries1);
+            this.chartControl1.ShowToolbar = true;
+            this.chartControl1.Size = new System.Drawing.Size(693, 392);
+            this.chartControl1.TabIndex = 0;
+            this.chartControl1.Text = "Users";
+            // 
+            // 
+            // 
+            this.chartControl1.Title.Name = "Default";
+            this.chartControl1.Titles.Add(this.chartControl1.Title);
+            this.chartControl1.ToolBar.Visible = true;
+            this.chartControl1.VisualTheme = "";
+            // 
+            // progressBarAdvUsersStatistics
+            // 
+            this.progressBarAdvUsersStatistics.BackMultipleColors = new System.Drawing.Color[] {
+        System.Drawing.Color.Empty};
+            this.progressBarAdvUsersStatistics.BackSegments = false;
+            this.progressBarAdvUsersStatistics.CustomText = null;
+            this.progressBarAdvUsersStatistics.CustomWaitingRender = false;
+            this.progressBarAdvUsersStatistics.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.progressBarAdvUsersStatistics.ForegroundImage = null;
+            this.progressBarAdvUsersStatistics.GradientEndColor = System.Drawing.SystemColors.ActiveCaption;
+            this.progressBarAdvUsersStatistics.GradientStartColor = System.Drawing.Color.Orange;
+            this.progressBarAdvUsersStatistics.Location = new System.Drawing.Point(0, 379);
+            this.progressBarAdvUsersStatistics.Margin = new System.Windows.Forms.Padding(10);
+            this.progressBarAdvUsersStatistics.MultipleColors = new System.Drawing.Color[] {
+        System.Drawing.Color.Empty};
+            this.progressBarAdvUsersStatistics.Name = "progressBarAdvUsersStatistics";
+            this.progressBarAdvUsersStatistics.ProgressStyle = Syncfusion.Windows.Forms.Tools.ProgressBarStyles.WaitingGradient;
+            this.progressBarAdvUsersStatistics.SegmentWidth = 12;
+            this.progressBarAdvUsersStatistics.Size = new System.Drawing.Size(693, 13);
+            this.progressBarAdvUsersStatistics.TabIndex = 15;
+            this.progressBarAdvUsersStatistics.Text = "progressBarAdv1";
+            this.progressBarAdvUsersStatistics.TextShadow = false;
+            this.progressBarAdvUsersStatistics.TextVisible = false;
+            this.progressBarAdvUsersStatistics.ThemeName = "WaitingGradient";
+            this.progressBarAdvUsersStatistics.TubeEndColor = System.Drawing.SystemColors.MenuHighlight;
+            this.progressBarAdvUsersStatistics.TubeStartColor = System.Drawing.Color.IndianRed;
+            this.progressBarAdvUsersStatistics.WaitingGradientWidth = 400;
+            // 
+            // DepartmentsStatisticsPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.progressBarAdvUsersStatistics);
+            this.Controls.Add(this.chartControl1);
+            this.Name = "DepartmentsStatisticsPage";
+            this.Size = new System.Drawing.Size(693, 405);
+            this.Controls.SetChildIndex(this.progressBarAdv3, 0);
+            this.Controls.SetChildIndex(this.chartControl1, 0);
+            this.Controls.SetChildIndex(this.progressBarAdvUsersStatistics, 0);
+            ((System.ComponentModel.ISupportInitialize)(this.progressBarAdv3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.progressBarAdvUsersStatistics)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Syncfusion.Windows.Forms.Chart.ChartControl chartControl1;
+        public Syncfusion.Windows.Forms.Tools.ProgressBarAdv progressBarAdvUsersStatistics;
+    }
+}
---
+++ Pages/ChartsPages/DepartmentsStatisticsPage.cs
@@ -0,0 +1,98 @@
+﻿using Spring.Pages.ChartsPages.ViewModel;
+using Spring.ViewControls.ViewChartsHelpers;
+using Syncfusion.Data.Extensions;
+using Syncfusion.Windows.Forms.Chart;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+
+
+namespace Spring.Pages.ChartsPages
+{
+    public partial class DepartmentsStatisticsPage : BaseChartPage
+    {
+        UsersStatisticsViewModel UsersStatisticsViewModel = new UsersStatisticsViewModel();
+        public DepartmentsStatisticsPage()
+        {
+            InitializeComponent();
+
+            //assign progressbar properties [visibility & Running for loading]
+            this.progressBarAdvUsersStatistics.DataBindings.Add(new Binding("Visible", UsersStatisticsViewModel, "LoadingChartData"));
+            this.progressBarAdvUsersStatistics.DataBindings.Add(new Binding("WaitingGradientEnabled", UsersStatisticsViewModel, "LoadingChartData"));
+
+            this.chartControl1.Series[0].ConfigItems.PieItem.DoughnutCoeficient = (float)(0.6);
+
+            this.chartControl1.Series[0].Points.Add(0D, ((double)(7D)));
+
+            for(int i = 0; i < this.chartControl1.Series[0].Points.Count; i++)
+            {
+                this.chartControl1.Series[0].Styles[i].Text = string.Format("Labor {0}%", this.chartControl1.Series[0].Points[i].YValues[0]);
+
+            }
+            this.Load += UsersStatisticsPage_Load;
+            UsersStatisticsViewModel.PropertyChanged += UsersStatisticsViewModel_PropertyChanged;
+            //this.VisibleChanged += UsersStatisticsPage_VisibleChanged;
+
+            
+        }
+
+        private void UsersStatisticsPage_VisibleChanged(object sender, System.EventArgs e)
+        {
+            UsersStatisticsViewModel.GetCountingAndStoreThem.Execute(true);
+        }
+
+        private void UsersStatisticsViewModel_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            //make sure we are in same VM and same property.
+            if (e.PropertyName == nameof(UsersStatisticsViewModel.LoadingChartData) && UsersStatisticsViewModel.GetType() == typeof(UsersStatisticsViewModel))
+            {
+                this.chartControl1.Series[0].Points.Clear();
+                this.chartControl1.Series[0].ResetSeriesModel();
+                 
+                this.chartControl1.Series[0].Style.ResetLabel();
+
+                foreach (string item in UsersStatisticsViewModel.AllCounters)
+                {
+                    this.chartControl1.Series[0].Points.Add(0D, double.Parse(item));
+                }
+                for (int i = 0; i < this.chartControl1.Series[0].Points.Count; i++)
+                {
+                    var list_point = this.chartControl1.Series[0].Points;
+                    List<double> yvals = new List<double>();
+                    foreach(ChartPoint point in list_point)
+                    {
+                        yvals.Add(point.YValues[0]);
+                    }
+                    var cumsum = yvals.ToList<double>().Sum();
+                    this.chartControl1.Series[0].Styles[i].Text = string.Format("{0} {1}%", UsersStatisticsViewModel.AllDepartments[i].Name, Convert.ToInt32((((this.chartControl1.Series[0].Points[i].YValues[0]) / cumsum) * 100)).ToString());
+
+                }
+            }
+
+        }
+       
+
+        private void UsersStatisticsPage_Load(object sender, System.EventArgs e)
+        {
+            UsersStatisticsViewModel.GetCountingAndStoreThem.Execute(true);
+        }
+        #region Methods Helpers
+        public override void SetViewName()
+        {
+            
+            #region view define
+            (StaticVM.VMCentral.DockingManagerViewModel.ViewName) = PagesChartsNodesNames.UsersChartTitle;
+            #endregion
+ 
+        }
+       
+       
+        #endregion
+    }
+}
+
+
